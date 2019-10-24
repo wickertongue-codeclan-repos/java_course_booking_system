8f78adc3014cef983c3796da4834761d1a6f7e97
hayden <haydenwb@gmail.com>
2019-10-24 13:19:59 +0100

    created find courses by star method, tests passing

--- src/main/java/com/codeclan/example/course_booking_system/repositories/courseRepository/CourseRepository.java
+++ src/main/java/com/codeclan/example/course_booking_system/repositories/courseRepository/CourseRepository.java
@@ -4,6 +4,12 @@ import com.codeclan.example.course_booking_system.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CourseRepository extends JpaRepository<Course, Long>, CourseRepositoryCustom {
+
+    List<Course> findCoursesByStarRating(int starRating);
+
+
 }
--- src/test/java/com/codeclan/example/course_booking_system/CourseBookingSystemApplicationTests.java
+++ src/test/java/com/codeclan/example/course_booking_system/CourseBookingSystemApplicationTests.java
@@ -1,13 +1,41 @@
 package com.codeclan.example.course_booking_system;
 
+import com.codeclan.example.course_booking_system.models.Course;
+import com.codeclan.example.course_booking_system.repositories.bookingRepository.BookingRepository;
+import com.codeclan.example.course_booking_system.repositories.courseRepository.CourseRepository;
+import com.codeclan.example.course_booking_system.repositories.customerRepository.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CourseBookingSystemApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
+	@Autowired
+	CourseRepository courseRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canGetAllCoursesWithARating() {
+		List<Course> foundCourses = courseRepository.findCoursesByStarRating(3);
+		assertEquals(1, foundCourses.size());
+		assertEquals("React", foundCourses.get(0).getName());
 	}
 
 }
